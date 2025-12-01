d40ed42e91fc935314e850d45442fc5395a7f71b
Kayo Vinicius <kayoribeirodev@gmail.com>
2025-12-01 11:39:10 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Coisas boas acontecem depois de coisas ruins oi teste</h1>
+    return <h1>Coisas boas acontecem depois de coisas ruins, oi amor</h1>
 }
 
 export default Home;
