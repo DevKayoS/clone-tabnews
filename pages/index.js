3875f0e6e4fdf347fb7e8a7f8063a25ce5c1d90d
Kayo <kayoribeirodev@gmail.com>
2025-12-08 23:23:43 -0300

    fix: arrumando mensagem da tela

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Coisas boas acontecem depois de coisas ruins, oi amor</h1>;
+  return <h1>Coisas boas acontecem depois de coisas ruins!</h1>;
 }
 
 export default Home;
