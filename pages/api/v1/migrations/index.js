256b08fc7ae3bd195e55959ed340942b20d18367
Kayo <kayoribeirodev@gmail.com>
2026-02-05 00:05:16 -0300

    feat: created main archives

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,10 +1,40 @@
 import migrationRunner from 'node-pg-migrate'
+import { join } from "node:path"
 
 export default async function migrations(request, response) {
-    const migrations = await migrationRunner({
 
+    let migrations
+    if (request.method === 'GET') {
+        migrations = await migrationRunner({
+            databaseUrl: process.env.DATABASE_URL,
+            dryRun: true,
+            dir: join("infra", "migrations"),
+            direction: "up",
+            verbose: true,
+            migrationsTable: "pgmigrations"
+        })
+        return response.status(200).json(migrations)
+    }
+
+    if (request.method === 'POST') {
+        migrations = await migrationRunner({
+            databaseUrl: process.env.DATABASE_URL,
+            dryRun: false,
+            dir: join("infra", "migrations"),
+            direction: "up",
+            verbose: true,
+            migrationsTable: "pgmigrations"
+        })
+
+        return response.status(200).json(migrations)
+
+    }
+
+    return response.status(405).json({
+        status: false,
+        code: 405,
+        msg: 'Method not Allowed'
     })
-    response.status(200).json([{}])
 }
 
 
