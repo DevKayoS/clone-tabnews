0b2e4957d442a085fa4f1396535dd5d1a5a5926f
Kayo <kayoribeirodev@gmail.com>
2026-04-01 00:11:29 -0300

    fix: adicionando os novos campos pra fazer a logica de estabilizar os teste

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -5,11 +5,13 @@ async function waitForAllService() {
 }
 
 async function waitWebServer() {
-    return await retry(async () => {
+    return await retry(async (bail, tryNumber) => {
+        console.log(tryNumber)
         const response = await fetch("http://localhost:3000/api/v1/status");
         await response.json();
     }, {
         retries: 100,
+        maxTimeout: 1000
     })
 }
 
