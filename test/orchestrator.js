85c5b7454c96ea3088d96ae4a1a8613f372c4d4e
Kayo <kayoribeirodev@gmail.com>
2026-04-02 01:03:20 -0300

    fix: ajustando o `orchestrator.js` para rodar o `npm test` de forma mais robusta

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -5,8 +5,7 @@ async function waitForAllService() {
 }
 
 async function waitWebServer() {
-    return await retry(async (bail, tryNumber) => {
-        console.log(tryNumber)
+    return await retry(async () => {
         const response = await fetch("http://localhost:3000/api/v1/status");
         await response.json();
     }, {
