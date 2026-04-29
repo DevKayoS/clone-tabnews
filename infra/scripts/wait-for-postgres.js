79594f7ebff33de6e8af33fb1f35b5849a95eec0
Kayo <kayoribeirodev@gmail.com>
2026-04-28 23:21:44 -0300

    feat: addes `lint:eslint:check` script with `ESlint`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -3,7 +3,7 @@ const { exec } = require("node:child_process");
 function checkPostgres() {
   exec(
     "docker exec postgres-dev pg_isready --host localhost",
-    (error, stdout, stderr) => {
+    (error, stdout) => {
       if (!stdout.includes("accepting connections")) {
         process.stdout.write(".");
         checkPostgres();
