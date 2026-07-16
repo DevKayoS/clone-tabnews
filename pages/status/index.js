fcad74818af2b88c9ae0cf2d541aaccf43a85327
Kayo <kayoribeirodev@gmail.com>
2026-07-15 22:53:44 -0300

    feat: iniciando a implementacao do frontend da tela de status

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,13 +1,27 @@
-function CapsLock(propriedades) {
-  console.log(propriedades);
-  return <h1>{propriedades.texto.toUpperCase()}</h1>
-}
+import useSWR from "swr";
+import fetchAPI from "utils/fetchData";
 
 export default function StatusPage() {
   return (
     <>
       <h1>Status</h1>
-      <CapsLock texto="esse texto vai ficar em caixa alta" />
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  })
+
+  console.log(data)
+  let updatedAtText = isLoading ? "Carregando..." : new Date(data.updated_at).toLocaleString("pt-br");
+
+  return (
+    <>
+      <div>Ultima Atualizacao: {updatedAtText}</div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
