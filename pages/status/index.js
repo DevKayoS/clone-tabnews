7bdb2daea443ba818a164127f4f3169789b55439
Kayo <kayoribeirodev@gmail.com>
2026-07-16 23:11:51 -0300

    refactor: refatorando a pagina de status para remover o json puro

--- pages/status/index.js
+++ pages/status/index.js
@@ -6,6 +6,7 @@ export default function StatusPage() {
     <>
       <h1>Status</h1>
       <UpdatedAt />
+      <Database />
     </>
   );
 }
@@ -15,13 +16,40 @@ function UpdatedAt() {
     refreshInterval: 2000,
   })
 
-  console.log(data)
   let updatedAtText = isLoading ? "Carregando..." : new Date(data.updated_at).toLocaleString("pt-br");
 
   return (
     <>
       <div>Ultima Atualizacao: {updatedAtText}</div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
+}
+
+
+function Database() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  })
+
+  console.log(data)
+  let databaseStatusInfo = "Carregando..."
+
+  if (!isLoading && data) {
+    const databaseInfo = data.dependencies.database
+    databaseStatusInfo = (
+      <>
+        <div>Versão: {databaseInfo.version}</div>
+        <div>Conexões abertas: {databaseInfo.opened_connections}</div>
+        <div>Conexões máximas: {databaseInfo.max_connections}</div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Database</h1>
+      {databaseStatusInfo}
+    </>
+  )
+
 }
