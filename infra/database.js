e7166d2386c87b9663efbee5364b4ca050e6cea0
Kayo <kayoribeirodev@gmail.com>
2026-07-28 23:31:36 -0300

    feat: implementando erro customizado na rota de status

--- infra/database.js
+++ infra/database.js
@@ -33,10 +33,10 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
+    console.error("Erro dentro do database.js", error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
