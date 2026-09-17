21978fdf2292c5395de87a2dc1ad5eeba2bf2584
Kayo <kayoribeirodev@gmail.com>
2026-09-17 01:28:40 -0300

    feat: implementando a primeira versao da `/api/v1/user`

--- models/user.js
+++ models/user.js
@@ -1,7 +1,16 @@
 import database from "infra/database";
+import { validate } from "node_modules/uuid/dist/cjs";
 
 async function create({ username, email, password }) {
+  await validateUniqueEmail(email);
 
+
+  const newUser = await runInsertQuery({ username, email, password })
+  return newUser;
+}
+
+
+async function runInsertQuery({ username, email, password }) {
   const newUser = await database.query({
     text: `
       INSERT INTO 
@@ -14,6 +23,26 @@ async function create({ username, email, password }) {
   })
 
   return newUser.rows[0]
+}
+
+
+async function validateUniqueEmail(email) {
+  const validateUniqueEmail = await database.query({
+    text: `
+      SELECT
+        email
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+    ;`,
+    values: [email]
+  })
+
+  if (validateUniqueEmail.rowsCount > 0)
+
+    return validateUniqueEmail.rows[0]
+
 }
 
 const user = {
--- test/integration/api/v1/users/post.test.js
+++ test/integration/api/v1/users/post.test.js
@@ -43,5 +43,59 @@ describe("POST /api/v1/users", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
     });
+
+    test("With duplicated `email`", async () => {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            username: 'johndoeduplicado',
+            email: 'johndoeduplicado@gmail.com',
+            password: 'senha123'
+          })
+        },
+      );
+
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(StatusCodes.CREATED);
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: 'johndoeduplicado',
+        email: 'johndoeduplicado@gmail.com',
+        password: 'senha123',
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at
+      })
+
+      expect(uuidVersion(responseBody.id)).toEqual(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            username: 'Johndoeduplicado2',
+            email: 'Johndoeduplicado@gmail.com',
+            password: 'senha123'
+          })
+        },
+      );
+
+      //const responseBody2 = await response2.json();
+      expect(response2.status).toBe(StatusCodes.BAD_REQUEST)
+
+    });
+
   });
 });
